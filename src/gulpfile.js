0334e3a5ef2d458a858bca3b567086553f541f5b
Grebenkov.Mikhail <grem305@hotmail.com>
2021-03-13 01:19:34 +0700

    fix

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,11 +1,23 @@
-const { src, dest, task, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass');
+const { src, dest, task, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
-// Компиляция файла в css 
+// Компиляция файла в css
 function buildSass() {
-    return src('src/scss/**/*.scss')
+  return src("src/scss/**/*.scss")
     .pipe(sass())
-    .pipe(dest('dist/css'))
+    .pipe(
+      postcss([
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 2 versions"],
+        }),
+        cssnano(),
+      ])
+    )
+    .pipe(dest("dist/css"));
 }
 
 exports.buildSass = buildSass;
