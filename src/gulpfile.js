35d0e98bde984d4b594cebde6f3b0393c4372c1a
Grebenkov.Mikhail <grem305@hotmail.com>
2021-03-13 01:58:06 +0700

    fix

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,6 +3,14 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+  browserSync.init({
+    server: 'src/',
+    notify: false,
+  })
+}
 
 // Компиляция файла в css
 function buildSass() {
@@ -17,7 +25,20 @@ function buildSass() {
         cssnano(),
       ])
     )
-    .pipe(dest("dist/css"));
+    .pipe(dest("dist/css"))
+    .pipe(dest("src/css"))
+    .pipe(browserSync.stream());
 }
 
-exports.buildSass = buildSass;
+function html() {
+  return src('src/**/*.html')
+    .pipe(dest('dist/'))
+    .pipe(browserSync.stream()) 
+}
+
+function serve() {
+  watch('src/scss/**/*.scss', buildSass);
+  watch('src/**/*.html', html)
+}
+
+exports.default = series(buildSass, parallel(browsersync, serve));
